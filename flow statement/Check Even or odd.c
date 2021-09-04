1fb93ea3abbeb61b4f1a78e9834dad4ac63d84ef
FRE4K_ <67673221+hiivbshsid@users.noreply.github.com>
2021-09-04 18:56:03 +0545

    Update Check Even or odd.c

--- flow statement/Check Even or odd.c
+++ flow statement/Check Even or odd.c
@@ -12,3 +12,18 @@
             
     return 0;
    }
+
+//even or odd using ternary operator///////
+#include<stdio.h>
+   int main(){
+       int num;
+     
+          printf("Enter a number:");
+          scanf("%d",&num);
+     
+         (num%2==0) ? 
+            printf("%d is Even Number.",num):
+            printf("%d is Odd Number.",num);
+            
+    return 0;
+   }
