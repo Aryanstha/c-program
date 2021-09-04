13fbedbdabb3cf4c4917068bfb8cdfe015f8768e
FRE4K_ <67673221+hiivbshsid@users.noreply.github.com>
2021-09-04 20:21:21 +0545

    Update factorial.c

--- flow statement/factorial.c
+++ flow statement/factorial.c
@@ -1,1 +1,33 @@
-
+// factorial of number using for loop.
+#include <stdio.h>
+int main(){
+           int i,num,fact=1;
+                
+                printf("Enter a number:");
+                scanf("%d",&num);
+                
+                for(i=1;i<=num;i++)
+                {
+                        fact=fact*i;
+                }
+                printf("Factorial of %d is: %d",num,fact);
+        return 0;        
+        }
+============================================================================================================================
+  // factorial of number using while loop.
+#include <stdio.h>
+          int main(){
+                  int i=1,num, fact=1;
+                  
+                  printf("Enter a number:");
+                  scanf("%d",&num);
+                  
+                  while(i<=num)
+                  {
+                          fact=fact*i;
+                          i++;
+                  }
+            printf("Factorial of %d is %d.",num,fact);
+           return 0;
+          }
+          
