c47bcbcb98da0de568e0640f506da3e5e498f5cf
FRE4K_ <67673221+hiivbshsid@users.noreply.github.com>
2021-09-04 18:55:08 +0545

    Create  to print 1st ten even number.c

---
+++ flow statement/to print 1st ten even number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+   int main(){
+           
+           int i=1, c=0;
+           
+           while(i<=10)
+           {
+            printf("%d\n",c);
+            i=i+1;
+            c=c+2;
+           }
+           return 0;
+   }
